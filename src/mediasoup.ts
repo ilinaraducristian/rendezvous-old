0f79444db26bd9ecfa9247e5c581edc3a8890030
Radu Ilina <ilinaraducristian@protonmail.com>
2021-08-02 07:36:12 +0300

    a MediaStream without tracks can't be attached to an AudioContext

--- src/mediasoup.ts
+++ src/mediasoup.ts
@@ -2,9 +2,15 @@ import {Device} from "mediasoup-client";
 
 const mediasoup = new Device();
 const remoteStream = new MediaStream();
-
 const audioContext = new AudioContext();
-audioContext.createMediaStreamSource(remoteStream).connect(audioContext.destination);
 
-export {remoteStream};
+let created = false;
+
+function createMediaStreamSource() {
+  if (created) return;
+  created = true;
+  audioContext.createMediaStreamSource(remoteStream).connect(audioContext.destination);
+}
+
+export {remoteStream, createMediaStreamSource};
 export default mediasoup;
