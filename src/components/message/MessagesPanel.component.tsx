856f62cd669e5ce0dd6c941ab811355ab83deefc
Radu Ilina <ilinaraducristian@protonmail.com>
2021-08-13 20:34:24 +0300

    update messages on scroll

--- src/components/message/MessagesPanel.component.tsx
+++ src/components/message/MessagesPanel.component.tsx
@@ -1,30 +1,71 @@
-import {useEffect, useRef, useState} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import PlusSVG from "../../svg/Plus.svg";
 import GIFSVG from "../../svg/GIF.svg";
 import MessageComponent from "./Message.component";
-import {useAppSelector} from "../../state-management/store";
-import {selectMessages, selectUsers,} from "../../state-management/slices/serversSlice";
+import {useAppDispatch, useAppSelector} from "../../state-management/store";
+import {
+  addMessages,
+  selectChannel as selectChannelAction,
+  selectMessages,
+  selectSelectedChannel,
+  selectUsers,
+} from "../../state-management/slices/serversSlice";
 import EmojiContainerComponent from "./EmojiContainer.component";
 import styled from "styled-components";
 import MessageInputComponent from "./MessageInput.component";
+import {useLazyGetMessagesQuery} from "../../state-management/apis/socketio";
+import config from "../../config";
 
 function MessagesPanelComponent() {
 
   const messagesList = useRef<HTMLDivElement>(null);
   const messages = useAppSelector(selectMessages);
   const users = useAppSelector(selectUsers);
+  const channel = useAppSelector(selectSelectedChannel);
   const [shortcut, setShortcut] = useState<string | null>(null);
   const emojiRef = useRef<any>(null);
+  const [fetch, {data, isSuccess, status}] = useLazyGetMessagesQuery();
+  const dispatch = useAppDispatch();
+  const [once, setOnce] = useState(false);
+  // const [offset, setOffset] = useState(2040);
+  const [offset, setOffset] = useState(0);
+  const [beginning, setBeginning] = useState(false);
 
   useEffect(() => {
     messagesList.current?.scroll(0, messagesList.current.scrollHeight);
-  }, [messages]);
+  }, [messages, once]);
+
+  useEffect(() => {
+    if (!isSuccess || status !== "fulfilled") return;
+    if (channel === undefined || data === undefined) return;
+    console.log(data);
+    if (data.length === 0) {
+      setBeginning(true);
+      return;
+    }
+    dispatch(addMessages(data));
+    dispatch(selectChannelAction(channel.id));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [status]);
+
+  const onScroll = useCallback((event) => {
+    if (channel === undefined) return;
+    if (messagesList.current?.scrollTop === 0) {
+      if (beginning) return;
+      if (!config.offline) {
+        console.log(offset);
+        fetch({serverId: channel.serverId, channelId: channel.id, offset: offset});
+        setOffset(offset + 30);
+        return;
+      }
+    }
+  }, [channel, fetch, offset, beginning]);
 
   return (
       <DivBodyMain>
-        <DivBodyMessages ref={messagesList}>
+        <DivBodyMessages ref={messagesList} onScroll={onScroll}>
           <Ol className="list">
-            {
+            {//.sort((m1, m2) => Date.parse(m1.timestamp) - Date.parse(m2.timestamp))
               messages?.map(message =>
                   <MessageComponent key={`message_${message.id}`}
                                     username={users.find(user => user.id === message.userId)?.username || ""}
