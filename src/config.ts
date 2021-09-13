eb6638160c066a10d834157fb16e09d8ed9a28a1
Radu Ilina <ilinaraducristian@protonmail.com>
2021-09-13 16:19:53 +0300

    update

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ const config = process.env.REACT_APP_ENVIRONMENT === "production" ? {
         backend: "https://rendezvous.myddns.me:30000/api",
         socketIoUrl: "https://rendezvous.myddns.me:30000",
         auth0: {
-            domain: "rendezvous-iam.eu.auth0.com",
-            client_id: "l1ZnZCOMXFKzMO0plbn0dl4o6Ijyg0se"
+            domain: "",
+            client_id: ""
         },
         keycloak: {
             url: "https://rendezvous.myddns.me:30000/auth",
