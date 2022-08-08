8ea2926cc068bc43b4c65cdebc7d6bfa7afcb872
Radu <ilinaraducristian@protonmail.com>
2022-08-08 08:16:21 +0300

    user email unique constraint

--- packages/nest/src/entities/user.schema.ts
+++ packages/nest/src/entities/user.schema.ts
@@ -7,15 +7,10 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-
-  id?: string;
-
-  _id?: Types.ObjectId;
-
   @Prop({ required: true })
   name: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, unique: true })
   email: string;
 
   @Prop({ required: true })
